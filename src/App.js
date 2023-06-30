6c701fee27a42642147d10386b2777ec1e50ceba
KevinLim62 <70590478+KevinLim62@users.noreply.github.com>
2023-06-30 09:09:03 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import NagivationBar from './NagivationBar';
 import ContentBar from './ContentBar';
+import { useEffect } from 'react';
 
 function App() {
+
+useEffect(() => {
+  const script = document.createElement('script');
+
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-ZRBJYF2E31";
+  script.async = true;
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-ZRBJYF2E31');
+  }
+  document.body.appendChild(script);
+
+  return () => {
+    document.body.removeChild(script);
+  }
+}, []);
+  
   return (
     <>
     <NagivationBar />
