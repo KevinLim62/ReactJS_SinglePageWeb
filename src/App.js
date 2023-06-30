395022e1c4161ce1ed9bd46ec298baa9ebbdc90e
KevinLim62 <70590478+KevinLim62@users.noreply.github.com>
2023-06-30 08:54:36 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import ContentBar from './ContentBar';
 function App() {
   return (
     <>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZRBJYF2E31"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-ZRBJYF2E31');
-      </script>
-
     <NagivationBar />
     <ContentBar />
     </>
